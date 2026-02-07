660e71947e77a1c55033d712f90b80cba73e7d2a
Santosh kumar <santosh30901@gmail.com>
2026-02-07 11:41:09 +0530

    logs

--- Roboshop.sh
+++ Roboshop.sh
@@ -3,17 +3,17 @@
 SG_ID="sg-03397cf5cc6b4017f"
 AMI_ID="ami-0220d79f3f480ecf5"
 
-for instance $@
+for instance in $@
 do
-   instance_id = $(aws ec2 run-instances --image-id $AMI_ID --instance-type "t3.micro" --security-group-id $SG_ID --tag_specifications "ResouceType=instance,Tags=[{key=name,Value=$instance}]" --query 'instances[0].InstanceId' --output text)
+   INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type "t3.micro" --security-group-id $SG_ID --tag_specifications "ResouceType=instance,Tags=[{key=name,Value=$instance}]" --query 'instances[0].InstanceId' --output text)
 
     if [ $instance == "frontend" ]; then
-       IP=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[].Instances[].PublicIpAddress' --output text )
+       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].PublicIpAddress' --output text )
 
     else
 
-      IP=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[].Instances[].PrivateIpAddress' --output text )
-      
+      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].PrivateIpAddress' --output text )
+
     fi
 
 done
