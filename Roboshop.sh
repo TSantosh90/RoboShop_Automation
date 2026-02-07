3ff3f0b92149c9fa47b5fbdc9d7496a8b1f80e49
Santosh kumar <santosh30901@gmail.com>
2026-02-07 11:47:55 +0530

    logs

--- Roboshop.sh
+++ Roboshop.sh
@@ -5,13 +5,13 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-   INSTANCE_ID=$(aws ec2 run-instances \
+   INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
      --security-group-id $SG_ID \
      --tag_specifications "ResouceType=instance,Tags=[{key=name,Value=$instance}]" \
      --query 'instances[0].InstanceId' \
-     --output text)
+     --output text )
 
     if [ $instance == "frontend" ]; then
        IP=$(
@@ -31,5 +31,4 @@ do
        )
 
     fi
-        echo "IP_Address = $IP"
 done
